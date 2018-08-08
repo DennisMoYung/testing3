d1219c888076fe47ea1b46eeba7e83710710b03b
dennismoyung <dennis.moyung@nikoyo.com>
2018-08-08 11:01:42 +0800

    open "test2.java" for add function

--- testjava/src/test1.java
+++ testjava/src/test1.java
@@ -1,9 +1,11 @@
 
 public class test1 {
-
+	
 	public static void main(String[] args) {
 	System.out.println("test");
-
+	test2 t2 = new test2();
+	int a = t2.add();
+	System.out.println(a);
 	}
 
 }
---
+++ testjava/src/test2.java
@@ -0,0 +1,13 @@
+
+public class test2 {
+	private int a;
+	private int b;
+	public test2 (){
+	a =1;
+	b=2;
+	}
+	
+	public int add() {
+		return a+b;
+	}
+}
