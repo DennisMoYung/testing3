c931b37589f0520f344bad31c9c30fb366f74b84
dennismoyung <dennis.moyung@nikoyo.com>
2018-08-08 11:20:49 +0800

    create test3.java with a-b

--- testjava/src/test1.java
+++ testjava/src/test1.java
@@ -4,7 +4,10 @@ public class test1 {
 	public static void main(String[] args) {
 	System.out.println("test");
 	test2 t2 = new test2();
+	test3 t3= new test3();
 	int a = t2.add();
+	System.out.println(a);
+	a = t3.reduce();
 	System.out.println(a);
 	}
 
---
+++ testjava/src/test3.java
@@ -0,0 +1,13 @@
+
+public class test3 {
+	private int a ,b;
+	
+	public test3(){
+		a =2;
+		b=1;
+	}
+	
+	public int reduce(){
+		return a-b;
+	} 
+}
